9c51bc0c5abc3c7df94c10f58cb094f27025b925
PinskayaNika <56188948+PinskayaNika@users.noreply.github.com>
2020-02-26 19:15:59 +0300

    feature: change min, max, avg funcs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
 exports.min = function min (array) {
-  return 0;
+  let mini = 1000;
+  if(array == undefined){
+	return 0;
+  } else
+  if (array.length == 0) {
+	return 0;
+  } else {
+	for (let i = 0; i < array.length; i++) {
+	  if (array[i] < mini) {
+	    mini = array[i];
+	  }
+	}
+	return mini;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxi = -1000;
+  if(array == undefined){
+	return 0;
+  } else
+  if (array.length == 0) {
+	return 0;
+  } else {
+	for (let i = 0; i < array.length; i++) {
+	  if (array[i] > maxi) {
+	    maxi = array[i];
+	  }
+	}
+	return maxi;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+ let avgs = 0;
+ let sum = 0;
+  if(array == undefined){
+	return 0;
+  } else
+  if (array.length == 0) {
+	return 0;
+  } else {
+	for (let i = 0; i < array.length; i++) {
+	  sum = sum + array[i];
+	}
+	avgs = sum / array.length;
+	return avgs;
+  }
+  //return 0;
 }
